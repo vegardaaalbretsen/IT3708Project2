144192d9a329ea0dcf66d02e9562d3d553c6f01b
Erlend Vitsø <669804@stud.hvl.no>
2026-02-19 14:33:40 +0100

    started to initialize ga + added test folder

---
+++ src/algorithms/ga.jl
@@ -0,0 +1,24 @@
+# Individual (chromosome): Vector{Int}
+#   [4,2,7,-1,1,6,-1,3,5]  -> routes: [4,2,7], [1,6], [3,5]
+#
+# Population: Vector{Vector{Int}}
+#   [
+#     [4,2,7,-1,1,6,-1,3,5],
+#     [2,5,-1,4,1,3,-1,7,6]
+#   ]
+
+function GA(
+        fitness_fn,
+        population::Vector{Vector{Int}},
+        max_generations::Int,
+        parent_selection_fn,
+        crossover_fn,
+        selection_fn
+)
+
+    # n = length(population)
+
+    # fitnesses = Vector{Vector{Float}}
+
+    # for g in 1:max_generations
+        # fitnesses = fitness_fn(pop, params(?))
