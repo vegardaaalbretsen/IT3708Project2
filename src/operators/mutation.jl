7d3cd2de5a24f3666796e66c5002cd165875b6c2
Vegard Aa Albretsen <111348081+vegardaaalbretsen@users.noreply.github.com>
2026-02-20 11:38:17 +0100

    SwapMutation implementation (#2)
    * Rename test to runtests as per Julia Best practise
    
    * Swap mutation test created, not checked
    
    * Swap mutation created, not tested. Needed some project.toml thingy
    
    * Moved files and fixed test
    
    * Swap mutation added and passes tests

--- src/operators/mutation.jl
+++ src/operators/mutation.jl
@@ -1,5 +1,3 @@
-# TODO
-
 """
 Mutation operators:
     Swap
@@ -8,4 +6,40 @@ Mutation operators:
     Inversion
 """
 
+
+
 abstract type Mutator end
+
+
+struct SwapMutator <: Mutator end
+"""
+    mutate(mutator::SwapMutator, individual)
+
+Randomly swap the position of two alleles in the `ind` candidate solution.
+"""
+function mutate(; mutator::SwapMutator, individual, rng)
+    new_ind = copy(individual)
+    n = length(new_ind)
+    i,j = rand(rng, 1:n, 2)
+
+    # If i or j hits -1, move one further
+    while new_ind[i] == -1
+        i = i == n ? 1 : i + 1
+    end
+
+    while new_ind[j] == -1
+        j = j == n ? 1 : j + 1
+    end
+    
+    # Ensure that i and j are not the same index
+    while i == j
+        j = rand(rng, 1:n)
+        while new_ind[j] == -1
+            j = j == n ? 1 : j + 1
+        end
+    end
+
+    # Swap
+    new_ind[i], new_ind[j] = new_ind[j], new_ind[i]
+    return new_ind
+end
